8ef46efaafaa15fd29483de7c67bd81c6066f6ae
lomadi <lomadi@mac.com>
2020-04-24 23:58:27 +0200

    add some more tests, read all schemas

--- dpace-metadata-rest-experiments.py
+++ dpace-metadata-rest-experiments.py
@@ -7,10 +7,26 @@ class MetadataFields:
         self.aep    = api_entry_point
         self.h      = header
 
-    def  getSchemas (self):
-        pass
+    def  schemas (self):
+        metadataschemas = []
+        stillPagesToRead = True
+        page = 0
+        while stillPagesToRead:
+            url = self.aep + 'core/metadataschemas?page=' + str(page)
+            r = requests.get(url, headers = h)
+            halrespone = json.loads(r.content)
+            totalPages = halrespone['page']['totalPages']
+            pageNumber = halrespone['page']['number']
+            if  '_embedded' in halrespone.keys(): 
+                for mds in halrespone['_embedded']['metadataschemas']:
+                    del mds ["_links"]
+                    metadataschemas.append (mds)
+            page = page + 1
+            if (page  >= totalPages):
+                stillPagesToRead = False
+        return metadataschemas
 
-    def  getMetadataFieldsForSchema (self, schema):
+    def metadataFieldsForSchema (self, schema):
         metadatafields = []
         stillPagesToRead = True
         page = 0
@@ -21,7 +37,6 @@ class MetadataFields:
             totalPages = halrespone['page']['totalPages']
             pageNumber = halrespone['page']['number']
             if  '_embedded' in halrespone.keys(): 
-                mdfs = halrespone['_embedded']['metadatafields']
                 for mdf in halrespone['_embedded']['metadatafields']:
                     del mdf ["_links"]
                     del mdf ["_embedded"]
@@ -31,6 +46,21 @@ class MetadataFields:
                 stillPagesToRead = False
         return metadatafields
 
+    def printMetadataFields (self, schema, metadatafields):
+        i = 1
+        for mf in metadatafields:
+            infostring = str(i) + ':  ' + schema + '.' + mf['element']
+            if ( type (mf['qualifier']) == str):
+                if len (mf['qualifier'])  > 0:
+                    infostring = infostring + '.'+ mf['qualifier']
+            if ( type (mf['scopeNote']) == str):
+                scopeNote = mf['scopeNote'].replace('\n', '')
+                infostring = infostring + ',   ' + scopeNote
+
+            print (infostring)
+            i = i+1
+    
+
 r = requests.post('http://dspace-rest.silicolab.bibbox.org/server/api/authn/login?user=v@bibbox.org&password=vendetta') 
 bearer_token = r.headers['Authorization']
 
@@ -38,18 +68,18 @@ h = {'Authorization':r.headers['Authorization']}
 
 mf =  MetadataFields ('http://dspace-rest.silicolab.bibbox.org/server/api/', h)
 
-mf.getSchemas()
-metadatafields = mf.getMetadataFieldsForSchema('slide')
-schema = 'dc'
-metadatafields = mf.getMetadataFieldsForSchema(schema)
+schemas = mf.schemas()
+print(json.dumps(schemas, indent=4, sort_keys=True))
 
-for mf in metadatafields:
-    infostring = schema + '.' + mf['element']
-    if ( type (mf['qualifier']) == str):
-        infostring = infostring + '.'+ mf['qualifier']
-    if ( type (mf['scopeNote']) == str):
-        scopeNote = mf['scopeNote'].replace('\n', '')
-        infostring = infostring + ',   ' + scopeNote
+for s in schemas:
+    prefix = s['prefix']
+    metadatafields = mf.metadataFieldsForSchema(prefix)
+    print ('=================== ' + prefix + ' ='+ '='*(25-len(prefix)) + ' ' + str(len(metadatafields)))
+    mf.printMetadataFields (prefix, metadatafields)
 
-    print (infostring)
+
+#schema = 'slide'
+#metadatafields = mf.metadataFieldsForSchema(schema)
+#mf.printMetadataFields (schema, metadatafields)
+#print(json.dumps(metadatafields, indent=4, sort_keys=True))
 
